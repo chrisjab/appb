1221ff93bcd6d0104d088a1cff226d19afc18ec4
bluemixtraining1 <bluemixtraining1@gmail.com>
2015-09-21 01:10:12 -0700

    copied A stuff to B

---
+++ src/main/java/au/com/bluemix/training/appb/IServiceB.java
@@ -0,0 +1,16 @@
+package au.com.bluemix.training.appb;
+
+import javax.ejb.Local;
+import javax.jws.WebService;
+import javax.jws.soap.SOAPBinding;
+import javax.jws.soap.SOAPBinding.ParameterStyle;
+import javax.jws.soap.SOAPBinding.Use;
+
+@Local
+@WebService(targetNamespace="http://appb/ServiceB")
+@SOAPBinding(style=javax.jws.soap.SOAPBinding.Style.RPC, use=Use.LITERAL, parameterStyle=ParameterStyle.WRAPPED)
+
+
+public interface IServiceB {
+	int plus(int left, int right);
+}
